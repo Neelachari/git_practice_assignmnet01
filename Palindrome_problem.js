f78f94c648659fa9f011f07c02c7610dbe5a8c70
Neelachari N H <neelachari34@gmail.com>
2022-10-19 12:10:57 +0530

    added the prime and palindrome problem

---
+++ Palindrome_problem.js
@@ -0,0 +1,13 @@
+let str="racecar";
+
+let pali="";
+
+for(let i=str.length-1;i>=0;i--){
+    pali+=str[i]
+}
+if(pali=!str){
+    console.log("Palindrome")
+}
+else{
+    console.log("Not a Palindrome")
+}
---
+++ prime_problem.js
@@ -0,0 +1,17 @@
+for(let i=1;i<=50;i++){
+    let count=0
+for(let j=0;j<=i;j++){
+    if(i%j==1){
+        count++
+    }
+}
+if(count!=2){
+    console.log(i,"is a Prime");
+}
+else{
+    console.log(i,"is Not a Prime")
+}
+
+}
+
+
